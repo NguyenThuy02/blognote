a97a8d3f48815a0a860ef5394148f80214f72d1b
ManhLH1 <108191135+ManhLH1@users.noreply.github.com>
2025-03-05 21:52:39 +0700

    update code

---
+++ app/layout.js
@@ -0,0 +1,49 @@
+import Sidebar from "../src/app/components/Sidebar";
+import Header from "../src/app/components/Header"; 
+import Footer from "../src/app/components/Footer";
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "../src/app/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Blog & Notes App",
+  description: "Smart blog and notes application",
+};
+
+function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex flex-col min-h-screen">
+          {/* Header */}
+          <Header />
+          <div className="flex flex-1">
+
+          {/* Sidebar */}
+
+            {/* Sidebar */}
+            <Sidebar />
+
+            {/* Main content */}
+            <main className="flex-1 p-6">{children}</main>
+          </div>
+
+          {/* Footer */}
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default Layout;
