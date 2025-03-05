e6f9065e625afb813c41393fbbfa8caf9363a37f
nguyệt thúy <144653260+NguyenThuy02@users.noreply.github.com>
2025-03-05 16:27:56 +0700

    no done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
+"use client";
+import React, { useState } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import Sidebar from "./components/Sidebar";
+import Login from "./components/login/login";
+import Register from "./components/login/register";
+import Link from "next/link";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,17 +18,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Blog & Notes App",
-  description: "Smart blog and notes application",
-};
-
 export default function RootLayout({ children }) {
+  const [showLogin, setShowLogin] = useState(false); // Di chuyển useState vào đây
+
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="mr-0">
+          <button onClick={toggleLogin}>
+            {showLogin}
+            Login
+          </button>
+
+          {showLogin && (
+            <div>
+              <Link href="/login">
+                <Register />
+              </Link>
+            </div>
+          )}
+        </div>
         <div className="flex">
           {/* Sidebar */}
           <Sidebar />
