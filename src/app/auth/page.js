3199c41e14dcb2ddde536544a55bd99d57f39379
nguyệt thúy <144653260+NguyenThuy02@users.noreply.github.com>
2025-03-15 10:14:33 +0700

    fix vercel

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Profile from "./Profile";
-import { UserOutlined } from "@ant-design/icons";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function AuthPage() {
+function App() {
   return (
     <Router>
-      <header>
-        <nav>
-          <ul style={{ display: "flex", listStyle: "none", padding: 0 }}>
-            <li style={{ marginRight: "20px" }}>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/profile">
-                <UserOutlined />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/profile" component={Profile} />
-        </Switch>
-      </main>
+      <Switch>
+        {/* */}
+        {/* */}
+      </Switch>
     </Router>
   );
 }
 
-export default AuthPage;
+export default App;
