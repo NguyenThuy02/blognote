87635015c654b47224b43baf9003a7e0a8d32e5e
nguyệt thúy <144653260+NguyenThuy02@users.noreply.github.com>
2025-03-15 10:32:51 +0700

    detail/page.js

--- src/app/blog/detail/page.js
+++ src/app/blog/detail/page.js
@@ -1,3 +1,4 @@
+/*
 "use client";
 import React from "react";
 import Image from "next/image";
@@ -5,10 +6,10 @@ import Image from "next/image";
 const PostDetail = ({ post }) => {
   return (
     <div className="container mx-auto p-5">
-      {/* Tiêu đề bài viết */}
+      {}
       <h1 className="text-4xl font-bold">{post.title}</h1>
 
-      {/* Thông tin tác giả và ngày đăng */}
+      {}
       <div className="flex items-center mt-2">
         <Image
           src={post.authorImage}
@@ -23,12 +24,12 @@ const PostDetail = ({ post }) => {
         </div>
       </div>
 
-      {/* Nội dung bài viết */}
+      { }
       <div className="mt-5">
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
 
-      {/* Tùy chọn chia sẻ bài viết */}
+      {}
       <div className="mt-5">
         <p>Chia sẻ bài viết:</p>
         <div className="flex space-x-4">
@@ -46,19 +47,19 @@ const PostDetail = ({ post }) => {
         </div>
       </div>
 
-      {/* Bình luận */}
+      {}
       <div className="mt-5">
         <h2 className="text-2xl">Bình luận</h2>
-        {/* Thêm mã cho mục bình luận ở đây */}
+        {}
       </div>
 
-      {/* Các bài viết liên quan */}
+      {}
       <div className="mt-5">
         <h2 className="text-2xl">Bài viết liên quan</h2>
-        {/* Danh sách bài viết liên quan */}
+        {}
       </div>
 
-      {/* Nút quay lại */}
+      {}
       <div className="mt-5">
         <button className="bg-blue-500 text-white px-4 py-2 rounded">
           Quay lại danh sách bài viết
@@ -69,3 +70,5 @@ const PostDetail = ({ post }) => {
 };
 
 export default PostDetail;
+
+*/
